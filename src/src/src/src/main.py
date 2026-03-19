bea0352261bc5d8a4fb15ef1fdf273ea652a069c
BrunaBBGT <bruna.bordini@terra.com.br>
2026-03-18 21:16:32 -0300

    Create main.py

---
+++ src/src/src/src/main.py
@@ -0,0 +1,12 @@
+Add file → Create new file
+
+Nome: src/main.py
+from gerar_frase import gerar_frase
+from gravar_com_progresso import gravar_com_progresso
+
+def main():
+    gerar_frase()
+    gravar_com_progresso()
+
+if __name__ == "__main__":
+    main()
